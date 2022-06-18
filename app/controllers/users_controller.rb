ae13bcb1f04e858a56aa501be57585f551985b8b
ms2138 <m38371@gmail.com>
2022-06-18 15:58:32 -0700

    Add pagination to user's show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
 
   def show
-    @posts = @user.posts
+    @pagy, @posts = pagy(@user.posts.order("created_at DESC"))
   end
 
   private
