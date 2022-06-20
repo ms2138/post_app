b2bf953e4727fac7d05f88f2ef6c0d771e3270f0
ms2138 <m38371@gmail.com>
2022-06-19 21:52:19 -0700

    Remove edit, index, show, and update actions.  Update CommentsController's destroy action redirect path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,9 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
-  before_action :set_post, only: %i[ show edit update new create destroy ]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = @post.comments.order("created_at")
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-    @comments = @post.comments.order("created_at DESC")
-  end
-
+  before_action :set_comment, only: %i[ destroy ]
+  before_action :set_post, only: %i[ new create destroy ]
   # GET /comments/new
   def new
     @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
   end
 
   # POST /comments or /comments.json
@@ -40,25 +25,12 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully updated." }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /comments/1 or /comments/1.json
   def destroy
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.html { redirect_to [@post, @comment], notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
     end
   end
