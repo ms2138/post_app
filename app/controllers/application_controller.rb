5da6d551b8b48fb13d4a1c5c0ed68cc86eab94ea
ms2138 <m38371@gmail.com>
2022-06-19 20:00:29 -0700

    Add override for Devise's after_sign_out_path_for and after_sign_in_path_for methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  private
+  
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    current_user
+  end
 end
