c4724d963f59f7ec19b7006fbd55c98775a095c5
ms2138 <m38371@gmail.com>
2022-07-10 14:56:38 -0700

    Add user id to comments table.  Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_26_062247) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_045747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_26_062247) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "feeds", force: :cascade do |t|
@@ -104,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_26_062247) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "feeds"
   add_foreign_key "posts", "users"
 end
