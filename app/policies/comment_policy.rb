6a328fa6704e305b551230b8afc993e02a13584a
ms2138 <m38371@gmail.com>
2022-07-10 15:00:40 -0700

    Add policy for comments index, create, new and destroy actions

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,8 +1,17 @@
 class CommentPolicy < ApplicationPolicy
-  class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  def index?
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def new?
+    create?
+  end
+
+  def destroy?
+    user.present? && user == record.user
   end
 end
