5e077cf4190769a0bd2971f2433c2f0c016af587
ms2138 <m38371@gmail.com>
2022-07-13 23:19:21 -0700

    Update Feed's name property minimum length to 1

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,5 @@
 class Feed < ApplicationRecord
   has_many :posts
   
-  validates :name, presence: true, uniqueness: true, length { minimum: 5, maximum: 100 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 100 }
 end
