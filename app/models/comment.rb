18727fc4096cdd7cdb5fcb18d61d1920770232f1
ms2138 <m38371@gmail.com>
2022-07-13 21:53:56 -0700

    Add validations for Comment's name and content properties.  Add length validation to Feed name property

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :content, presence: true
 end
